e17a1dc3990b187ba07ec21f21e9c2162ea36f5d
Skiln3zz <guigui.puh@gmail.com>
2023-10-11 14:38:49 +0200

    found-min

--- src/array/found-min/found-min.js
+++ src/array/found-min/found-min.js
@@ -14,4 +14,32 @@ You can't use the function Math.min()
 
 // TODO add your code here
 
-module.exports = min;
+/*module.exports = function findMin(arr) {
+    if (!arr || arr.length === 0) {
+      return null;
+    }
+  
+    let minValue = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < minValue) {
+        minValue = arr[i];
+      }
+    }
+  
+    return minValue;
+  };
+  */
+  const min = (array) => {
+   if (!array || array.lenght === 0) return null;
+   console.log(array)
+   let min = array[0];
+   for (let i = l; i < array.lenght; i++) {
+    if (min>array[i]) {
+        min = array[i]
+    }
+   }
+   return min;
+  }
+
+  module.exports = min;
+
